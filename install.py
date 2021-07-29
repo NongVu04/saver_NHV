568c48974cb4b1985856112e78a3384324a7a740
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-07-29 14:05:19 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,10 @@
 import os,sys
 import requests
+os.system('pip install requests')
 print('Hãy đảm bảo rằng mạng của bạn ổn đinh!')
 print("Hacker sẽ yêu cầu cấp quyền nếu bạn là người mới!!")
 ip=input("Bạn là người mới hay cũ [moi] hay [cu]: ")
-saver=requests.get('https://pastebin.com/raw/gs1sP0JG')
+saver=requests.get('https://pastebin.com/raw/gs1sP0JG').txt
 if saver=='on':
     print('saver hoạt động')
 else:
