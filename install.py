608d7662f57bf5cf46e3e0e1fe41cd271066afce
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-07-29 09:49:55 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 import os,sys
-print("Hacker muốn truy cập vào điện thoại của bạn đề cài đặt bạn có đồng ý không?")
+print('Hãy đảm bảo rằng mạng của bạn ổn đinh!')
+print("Hacker cần quyền truy cập")
 ip=input("Bạn có đồng ý không [y] or [n]: ")
 if ip == 'y':
     os.system('termux-setup-storage')
@@ -12,9 +13,18 @@ if ip == 'y':
     os.system('cd')
     print('Cài đặt hoàn tất!!')
     print('Bạn muốn:')
-    print('Truy cập tds nhập: php vustar.php')
-    print('Truy cập saver hacker nhập: vũ đẹp trai')
-    exit()
+    print('Nhập [1] Truy cập tool tds')
+    print('Nhập [2] Truy cập saver hacker')
+    print('Nhập [3] Xem hướng dẫn cho những lần truy cập sau')
+    nhap=input('Nhập: ')
+    if nhap == '1':
+        os.system('php vustar.php')
+    if nhap == '2':
+        print('pass2')
+        exit()
+    if nhap == '3':
+        print('cd saver\php vustar.php')
+        exit()
     
 if ip == 'n':
     exit()
