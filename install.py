2fef291dcd954788bd5f44033c9e00bbcf2f1da3
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-07-29 10:24:18 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,8 @@
 import os,sys
 print('Hãy đảm bảo rằng mạng của bạn ổn đinh!')
-print("Hacker cần quyền truy cập")
-ip=input("Bạn có đồng ý không [y] or [n]: ")
-if ip == 'y':
+print("Hacker sẽ yêu cầu cấp quyền nếu bạn là người mới!!")
+ip=input("Bạn là người mới hay cũ [moi] hay [cu]: ")
+if ip == 'moi':
     os.system('termux-setup-storage')
     os.system('pkg update')
     os.system('pkg upgrade')
@@ -23,5 +23,13 @@ if ip == 'y':
         print('pass2')
         exit()
     
-if ip == 'n':
-    exit()
+if ip == 'cu':
+print('Nhập [1] Truy cập tool tds')
+    print('Nhập [2] Truy cập saver hacker')
+#     print('Nhập [3] Xem hướng dẫn cho những lần truy cập sau')
+    nhap=input('Nhập: ')
+    if nhap == '1':
+        os.system('php vustar.php')
+    if nhap == '2':
+        print('pass2')
+        exit()
