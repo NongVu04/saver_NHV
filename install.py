b682b2884773ad924e493989ee6e3b396c5350ed
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-05 22:11:58 +0700

    Update install.py

--- install.py
+++ install.py
@@ -64,7 +64,7 @@ if ip == 'cu':
     print_tex=requests.get("https://pastebin.com/raw/0BcauPav").text
     saver_1=requests.get('https://pastebin.com/raw/gs1sP0JG').text
     if saver_1 == 'on':
-            print(sv0)
+            print(svo)
     else:
         print(sv)
         exit()
