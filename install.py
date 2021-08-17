f5796ea68bc05e4b149e891ebcc20b54cfc7ea9c
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-17 16:15:07 +0700

    Update install.py

--- install.py
+++ install.py
@@ -19,7 +19,7 @@ if saver_1== 'on':
 else:
     print(sv)
     exit()
-print('Phiên Bản V1.1:')
+print('Phiên Bản V1.1')
 print("Ghi Chú: "+print_tex)
 print('Nhập [0] Để update tool')
 print('Nhập [1] Truy cập tool tds php (Bảo trì)')
@@ -27,20 +27,12 @@ print('Nhập [2] Truy cập công cụ hacker')
 print('Nhập [3] Truy cập tool tds python')
 nhap=input('Nhập: ')
 if nhap=='0':
-    os.system('cd&&rm -rf saver_NHV&&git clone https://github.com/NongVu04/saver_NHV.git&&cd saver_NHV&&termux-setup-storage&&pkg install php&&python install.py')
+    os.system('cd&&rm -rf saver_NHV&&git clone https://github.com/NongVu04/saver_NHV.git&&cd saver_NHV&&termux-setup-storage&&pkg install php&&bash tmux_setup&&bash setup&&python install.py')
 if nhap == '1':
     # os.system('php vustar.php')
     print('bảo trì!!')
     exit()
 if nhap == '2':
-    nx=input('Bạn có muốn cài chương trình không? (y/n): ')
-    if nx =='y':
-        os.system('bash tmux_setup')
-        os.system('bash setup')
         os.system('bash nexphisher')
-    if nx == 'n':
-        os.system('bash nexphisher')
-    else:
-        exit("Bạn chưa nhập gì")
 if nhap == '3':
     os.system('python vupy.py')
