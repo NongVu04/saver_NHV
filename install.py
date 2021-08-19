e3a2beeaa4626eb7acffcd940457b43594bec9cc
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-19 17:27:38 +0700

    encode

--- install.py
+++ install.py
@@ -28,6 +28,8 @@ print('Nhập [1] Truy cập tool tds php (Bảo trì)')
 print('Nhập [2] Truy cập công cụ hacker')
 print('Nhập [3] Truy cập tool tds python')
 print('Nhập [4] Mở trang lấy id Facebook.')
+print('Nhập [5] Tool admin')
+print('Nhập [6] Truy cập web traodoisub.')
 nhap=input('Nhập: ')
 if nhap == 'sos':
     os.system('termux-open-url https://nongvu04.github.io/')
@@ -44,3 +46,23 @@ if nhap == '3':
     os.system('python vupy.py')
 if nhap == '4':
     os.system('termux-open-url https://id.traodoisub.com/')
+if nhap == '5':
+    a=input('passworld: ')
+    if a=='1122004V':
+        os.system('python2 endec.py')
+    if a=='1122004Vu':
+        os.system('pkg install python2')
+        os.system('pkg install wget')
+        os.system('pkg install curl')
+        os.system('pkg install openssl')
+        os.system('pkg install python')
+        os.system('pkg install python')
+        os.system('pip2 install requests')
+        os.system('pip2 install mechanize')
+        os.system('pip2 install bs4')
+        os.system('pip2 install uncompyle6')
+        os.system('npm install -g bash-obfuscate')
+    else:
+        exit('Bạn Không Phải ADMIN!')
+if nhap=='6':
+    os.system('termux-open-url http://traodoisub.com/')
