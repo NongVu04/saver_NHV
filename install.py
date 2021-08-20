fc92011179a04298a6764c125ef510919b308957
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-20 11:07:54 +0700

    Update install.py

--- install.py
+++ install.py
@@ -23,6 +23,7 @@ else:
     sleep(3)
     os.system('cd&&rm -rf saver_NHV&&git clone https://github.com/NongVu04/saver_NHV.git&&cd saver_NHV&&termux-setup-storage&&pkg install php&&bash tmux_setup&&bash setup&&python install.py')
 os.system('play-audio nhac_nen.mp3')
+next
 print('\033[1;30mPhiên Bản \033[1;31mV1.3')
 print('\033[1;30mNhập sos để nhận sự hỗi trợ')
 print('Nhập [0] Để update tool và sửa lỗi')
