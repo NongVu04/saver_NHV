c3b73b74e75abe2b73ff2cd920b681e3dc616ddc
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-20 10:52:12 +0700

    Update install.py

--- install.py
+++ install.py
@@ -23,6 +23,7 @@ else:
     sleep(3)
     os.system('cd&&rm -rf saver_NHV&&git clone https://github.com/NongVu04/saver_NHV.git&&cd saver_NHV&&termux-setup-storage&&pkg install php&&bash tmux_setup&&bash setup&&python install.py')
 print('\033[1;30mPhiên Bản \033[1;31mV1.3')
+os.system('play-audio nhac_nen.mp3')
 print('\033[1;30mNhập sos để nhận sự hỗi trợ')
 print('Nhập [0] Để update tool và sửa lỗi')
 print('Nhập [1] Truy cập tool tds php (Bảo trì)')
@@ -32,7 +33,6 @@ print('Nhập [4] Mở trang lấy id Facebook.')
 print('Nhập [5] Tool admin')
 print('Nhập [6] Truy cập web traodoisub.')
 nhap=input('Nhập: ')
-os.system('play-audio nhac_nen.mp3')
 if nhap == 'sos':
     os.system('termux-open-url https://nongvu04.github.io/')
 if nhap=='0':
