d185aa01265dae515c51353214790df9e890a26d
Nong-Vu <87925288+NongVu04@users.noreply.github.com>
2021-08-21 16:01:35 +0700

    Update install.py

--- install.py
+++ install.py
@@ -2,8 +2,8 @@ import os,sys,re,json
 from time import sleep
 from random import randint
 print('\033[1;32m')
-os.system('play ting.mp3')
 os.system('pip install requests')
+os.system('play ting.mp3')
 import requests
 sv="""\033[1;31m
 ╔══════════════════════════════════════════════════════╗
@@ -27,7 +27,7 @@ else:
     os.system('cd&&rm -rf saver_NHV&&git clone https://github.com/NongVu04/saver_NHV.git&&cd saver_NHV&&termux-setup-storage&&pkg install sox -y&&pkg install php&&bash tmux_setup&&bash setup&&python install.py')
 
 print('\033[1;30mPhiên Bản \033[1;31mV1.4')
-print('\033[1;30mNhập sos để nhận sự hỗi trợ')
+print('\033[1;30mNhập [sos] để nhận sự hỗi trợ')
 print('\033[1;31mNhập [00] Để Thoát\033[1;30m')
 print('Nhập [0] Để update tool và sửa lỗi')
 print('Nhập [1] Truy cập tool tds php (New v2021)')
@@ -39,7 +39,7 @@ print('Nhập [6] Truy cập web traodoisub.')
 print('Nhập [7] Nghe nhạc + Ảo tưởng mình là hacker.')
 nhap=input('Nhập: ')
 if nhap =='00':
-    exit('Đã thoát!!!\n')
+    exit()
 if nhap == 'sos':
     os.system('termux-open-url https://nongvu04.github.io/')
     os.system('play xoa.mp3')
